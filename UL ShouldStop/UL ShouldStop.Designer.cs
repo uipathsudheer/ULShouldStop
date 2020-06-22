7095bd4cd437c70f89621612166b398e3ea95b97
uipathsudheer <42303684+uipathsudheer@users.noreply.github.com>
2020-06-22 07:45:20 +0530

    Add files via upload

---
+++ UL ShouldStop/UL ShouldStop.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace UL_Assistant
+{
+    partial class ShouldStop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShouldStop));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Image = global::UL_ShouldStop.Properties.Resources.stop;
+            this.btnStop.Location = new System.Drawing.Point(219, 112);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(20, 20);
+            this.btnStop.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.btnStop, "Click to Stop Current Running Process");
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(27, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop Current Process ";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UL_ShouldStop.Properties.Resources.ul;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(91, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(202, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "UiPath Learner";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // ShouldStop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(287, 176);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ShouldStop";
+            this.Text = "UL SholdStop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ UL ShouldStop/UL ShouldStop.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.IO;
+
+namespace UL_Assistant
+{
+    public partial class ShouldStop : Form
+    {
+        public ShouldStop()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+
+            //notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+            //notifyIcon.BalloonTipText = "Welcome to TutorialsPanel.com!!";
+            //notifyIcon.BalloonTipTitle = "Welcome Message";
+            //notifyIcon.ShowBalloonTip(2000);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            DialogResult dr = MessageBox.Show("Do you want to Stop '" + clickedButton.Name + "' Process ?", "Stop Process", MessageBoxButtons.YesNo);
+
+
+
+            if (dr == DialogResult.Yes)
+            {
+                
+            
+                
+                Process proc = null;
+                try
+                {
+                    string batDir = string.Format(@"C:\Users\user\Desktop\");
+                    proc = new Process();
+                    proc.StartInfo.WorkingDirectory = batDir;
+                    proc.StartInfo.FileName = "Yes.bat";
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    proc.Start();
+                    proc.WaitForExit();
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace.ToString());
+                }
+            }
+            if (dr == DialogResult.No)
+            {
+                MessageBox.Show("You Clicked on No");
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if (this.WindowState == FormWindowState.Minimized)
+            //{
+            //    Hide();
+            //    notifyIcon.Visible = true;
+            //}
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            // Path.GetTempPath();
+            DialogResult dr = MessageBox.Show( "Do You want to Stop ?", "Confirmation", MessageBoxButtons.YesNo);
+            if ( dr == DialogResult.Yes)
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(Path.GetTempPath() + "shouldstop.txt"))
+                {
+                    file.WriteLine("ULBOT");                    
+                }
+                MessageBox.Show("Stopped Successfully.");
+            }
+        }
+    }
+}
